d54a3789849fa2653fb64c6823f04e0d8d46cd5d
stacey <1174452667@qq.com>
2021-11-06 16:49:34 +0800

    refacotr: 将class组件写成函数式组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,37 +1,35 @@
-import { Component } from "react";
+import React from "react";
 import "antd/dist/antd.css";
 import { Input, Button, List } from "antd";
 
-class TodoListUI extends Component {
-  render() {
-    const { imputValue, listData, onChangeInput, onClickButton, onClickItems } =
-      this.props;
-    return (
-      <>
-        <Input
-          placeholder={imputValue}
-          value={imputValue}
-          onChange={onChangeInput}
-        />
-        <Button type="primary" onClick={onClickButton}>
-          增加
-        </Button>
-        <List
-          bordered
-          dataSource={listData}
-          renderItem={(item, index) => (
-            <List.Item
-              onClick={() => {
-                onClickItems(index);
-              }}
-            >
-              {item}
-            </List.Item>
-          )}
-        />
-      </>
-    );
-  }
-}
+const TodoListUI = (props) => {
+  const { imputValue, listData, onChangeInput, onClickButton, onClickItems } =
+    props;
+  return (
+    <>
+      <Input
+        placeholder={imputValue}
+        value={imputValue}
+        onChange={onChangeInput}
+      />
+      <Button type="primary" onClick={onClickButton}>
+        增加
+      </Button>
+      <List
+        bordered
+        dataSource={listData}
+        renderItem={(item, index) => (
+          <List.Item
+            onClick={() => {
+              onClickItems(index);
+            }}
+          >
+            {item}
+          </List.Item>
+        )}
+      />
+    </>
+  );
+};
 
 export default TodoListUI;
